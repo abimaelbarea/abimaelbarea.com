1901d15a843dc7a14a44ca3ddcdb5c58c444b9b8
Abimael Barea <elabi3@gmail.com>
2022-02-27 19:30:46 +0000

    feat(MDX): improvements on article styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,23 @@
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable react/jsx-key */
+
 import type { AppProps } from "next/app";
 import Link from "next/link";
+import "../styles/app.css";
 import "../styles/globals.css";
 import "../styles/theme.css";
 
 // how to make to themes??
 // variables css - find a good way to structure
-// Move the CSS code to a module linked to this file
 
 // Add newsletter
 // Add about
-// Include font icons 
+// Include font icons
 
-function MyApp({ Component, pageProps }: AppProps) {
+const Header = () => {
   return (
-    <>
-      <header>
+    <header className="app__header">
+      <nav>
         <Link href="/">Abimael Barea</Link>
         <div>
           <Link href="/blog">Blog</Link>
@@ -26,30 +29,44 @@ function MyApp({ Component, pageProps }: AppProps) {
           </a>
           <div>Theme</div>
         </div>
-      </header>
-      <main>
+      </nav>
+    </header>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="app_footer">
+      <nav>
+        <a href="https://twitter.com/abimaelbarea" target="_blank">
+          Twitter
+        </a>
+        <a href="https://www.linkedin.com/in/abimaelbarea" target="_blank">
+          Linkedin
+        </a>
+        <a href="https://github.com/abimaelbarea" target="_blank">
+          Github
+        </a>
+        <a href="https://abimaelbarea.medium.com/" target="_blank">
+          Medium
+        </a>
+        <a href="https://dev.to/abimaelbarea" target="_blank">
+          Dev.to
+        </a>
+      </nav>
+    </footer>
+  );
+};
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div className="app">
+      <Header />
+      <main className="app__content">
         <Component {...pageProps} />
       </main>
-      <footer>
-        <nav>
-          <a href="https://twitter.com/abimaelbarea" target="_blank">
-            Twitter
-          </a>
-          <a href="https://www.linkedin.com/in/abimaelbarea" target="_blank">
-            Linkedin
-          </a>
-          <a href="https://github.com/abimaelbarea" target="_blank">
-            Github
-          </a>
-          <a href="https://abimaelbarea.medium.com/" target="_blank">
-            Medium
-          </a>
-          <a href="https://dev.to/abimaelbarea" target="_blank">
-            Dev.to
-          </a>
-        </nav>
-      </footer>
-    </>
+      <Footer />
+    </div>
   );
 }
 
