aade94ebe16e38a630c4ca048dd622ca091eb55f
Abimael Barea <elabi3@gmail.com>
2022-03-03 21:47:34 +0000

    feat(blog): style improvements

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,55 @@
 /* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable react/jsx-key */
 
+import {
+  faDev,
+  faGithub,
+  faGithubSquare,
+  faLinkedin,
+  faMedium,
+  faTwitter
+} from "@fortawesome/free-brands-svg-icons";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { AppProps } from "next/app";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import styles from "../styles/app.module.css";
 import "../styles/globals.css";
 import "../styles/theme.css";
 
-const Header = () => {
+type Theme = "light" | "dark";
+
+type HeaderProps = {
+  onThemeChange: (newTheme: Theme) => void;
+};
+
+const Header = ({ onThemeChange }: HeaderProps) => {
+  const [theme, setTheme] = useState(false);
+
+  useEffect(() => {
+    onThemeChange(theme ? "light" : "dark");
+  }, [theme]);
+
   return (
     <header className={styles.appHeader}>
+      <Link href="/">Abimael Barea</Link>
       <nav>
-        <Link href="/">Abimael Barea</Link>
-        <div className="app__header-menu">
-          <Link href="/blog">Blog</Link>
-          <a
-            href="https://github.com/abimaelbarea/abimaelbarea.com"
-            target="_blank"
-          >
-            Repo
-          </a>
-          <div>Theme</div>
-        </div>
+        <Link href="/blog">Blog</Link>
       </nav>
+      <div className={styles.appHeaderActions}>
+        <a
+          href="https://github.com/abimaelbarea/abimaelbarea.com"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faGithubSquare} />
+        </a>
+        <button onClick={() => setTheme(!theme)}>
+          <FontAwesomeIcon icon={theme ? faSun : faMoon} />
+        </button>
+      </div>
     </header>
   );
 };
@@ -32,21 +57,21 @@ const Header = () => {
 const Footer = () => {
   return (
     <footer className={styles.appFooter}>
-      <nav>
+      <nav className={styles.appFooterNav}>
         <a href="https://twitter.com/abimaelbarea" target="_blank">
-          Twitter
+          <FontAwesomeIcon icon={faTwitter} />
         </a>
         <a href="https://www.linkedin.com/in/abimaelbarea" target="_blank">
-          Linkedin
+          <FontAwesomeIcon icon={faLinkedin} />
         </a>
         <a href="https://github.com/abimaelbarea" target="_blank">
-          Github
+          <FontAwesomeIcon icon={faGithub} />
         </a>
         <a href="https://abimaelbarea.medium.com/" target="_blank">
-          Medium
+          <FontAwesomeIcon icon={faMedium} />
         </a>
         <a href="https://dev.to/abimaelbarea" target="_blank">
-          Dev.to
+          <FontAwesomeIcon icon={faDev} />
         </a>
       </nav>
     </footer>
@@ -56,10 +81,12 @@ const Footer = () => {
 const queryClient = new QueryClient();
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [theme, setTheme] = useState<Theme>();
+
   return (
     <QueryClientProvider client={queryClient}>
-      <div className={styles.app}>
-        <Header />
+      <div className={`${styles.app} ${theme}`}>
+        <Header onThemeChange={(newTheme: Theme) => setTheme(newTheme)} />
         <main className={styles.appContent}>
           <Component {...pageProps} />
         </main>
