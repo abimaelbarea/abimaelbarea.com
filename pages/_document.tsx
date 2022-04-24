a8cffe81f8bc0eab4f22ddb582ba40a1951eaf96
Abimael Barea <elabi3@gmail.com>
2022-04-24 13:19:37 +0100

    feat(pwa): adding pwa capabilities

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,31 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400;1,500&display=swap" rel="stylesheet"/>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          />
+          <meta name="theme-color" content="#1C1C1C" />
+          <link rel="icon" type="image/png" href="/icons/favicon.ico" />
+          <link
+            href="/icons/16.png"
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+          />
+          <link
+            href="/icons/32.png"
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+          />
+          <link rel="apple-touch-icon" href="icons/apple-icon.png"></link>
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400;1,500&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
