0dcbb18d6a17e08c2e0dff9e6e2b4546cd64767e
Abimael Barea <elabi3@gmail.com>
2022-05-07 17:38:26 +0100

    feat(improvements): pwa icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,19 +8,10 @@ class MyDocument extends Document {
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#1C1C1C" />
           <link rel="icon" type="image/png" href="/icons/favicon.ico" />
-          <link
-            href="/icons/16.png"
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-          />
-          <link
-            href="/icons/32.png"
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-          />
-          <link rel="apple-touch-icon" href="icons/apple-icon.png"></link>
+          <link rel="apple-touch-icon" sizes="120x120" href="/icons/128.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/icons/152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/icons/180.png" />
+          <meta name="apple-mobile-web-app-title" content="Abimael" />
           <link rel="manifest" href="/manifest.json" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400;1,500&display=swap"
